3a272c7ec5171da5758129414b93aeb58db7871e
Huỳnh Phú Quí <hphuqui33@gmail.com>
2026-05-15 20:28:40 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import './App.css'
 
 
 // () => { }
+
+const MyComponent = () => {
+  return (
+    <div>eric & hoidanit update</div>
+  );
+}
+
+
+
 const App = () => {
   const [count, setCount] = useState(0)
 
-  // function myFunction(){
-  //   console.log(">>> run my function")
-  // }
-
-  const myFunction = () => {
-    console.log("run my arrow function")
-  }
-
-  myFunction();
+ 
 
   return (
     <>
@@ -29,6 +30,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello world newww</h1>
+      <MyComponent></MyComponent>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
