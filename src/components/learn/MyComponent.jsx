f4c28384c5975a36267c672afaf46e2d527fbd5f
Huỳnh Phú Quí <hphuqui33@gmail.com>
2026-05-15 21:17:28 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,16 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = "eric 1"; //string
+    // const hoidanit = 25; //string
+    const hoidanit = {
+        name: "hoidanit",
+        age: 25
+    }
   return (
     <>
-    <div className="">eric & hoidanit update</div>
+    <div className="">{JSON.stringify(hoidanit)} & hoidanit update</div>
+    <div>{console.log("ERIC")}</div>
     <div className="child" style={{borderRadius: "10px"}}>child</div>
     </>
     
